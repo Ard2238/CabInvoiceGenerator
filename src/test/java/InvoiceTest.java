43130925a5b905dd3d96410e04afcc224290c2ee
Ard2238 <abhishekrdas28@gmail.com>
2020-10-29 07:43:43 +0530

    Refactored Code to get invoice when given userId

--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -3,6 +3,7 @@ import org.junit.Test;
 
 public class InvoiceTest {
     CalculateFare calculateFare = new CalculateFare();
+    RideRepository repo = new RideRepository();
 
     @Test
     public void testCalculateTotalFare(){
@@ -23,8 +24,22 @@ public class InvoiceTest {
                         new Ride(3,2)};
         //Assert.assertEquals(82,calculateFare.getTotalFare(ride),0);
 
-        InvoiceSummary expectedinvoiceSummary = new InvoiceSummary(3,82);
-        InvoiceSummary actualinvoiceSummary = calculateFare.getTotalFare(ride);
-        Assert.assertEquals(expectedinvoiceSummary, actualinvoiceSummary);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(3,82);
+        InvoiceSummary actualInvoiceSummary = calculateFare.getTotalFare(ride);
+        Assert.assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
+    }
+
+    @Test
+    public void testInvoiceService(){
+        Ride[] ride1 = {new Ride(3.5, 10),new Ride(0.3,1),new Ride(3,2)};
+        InvoiceSummary setInvoice1 = calculateFare.getTotalFare(ride1);
+        repo.rideRepo.put("001", setInvoice1);
+        Ride[] ride2 = {new Ride(2, 5),new Ride(0.7,0),new Ride(6,1)};
+        InvoiceSummary setInvoice2 = calculateFare.getTotalFare(ride1);
+        repo.rideRepo.put("002", setInvoice2);
+
+        InvoiceSummary actual = new InvoiceSummary(3, 82);
+        InvoiceSummary expected = repo.rideRepo.get("001");
+        Assert.assertEquals(expected, actual);
     }
 }
