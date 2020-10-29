427458a63dc741e676b83f064f29ccc43987a228
Ard2238 <abhishekrdas28@gmail.com>
2020-10-29 06:23:57 +0530

    Added Code and tests to calculate minimum fare

--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -2,5 +2,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceTest {
-
+    @Test
+    public void testCalculateTotalFare(){
+        CalculateFare calculateFare = new CalculateFare();
+        Assert.assertEquals(45, calculateFare.getTotalFare(3.5,10), 0);
+        Assert.assertEquals(5, calculateFare.getTotalFare(0.3,1),0);
+        Assert.assertEquals(-1, calculateFare.getTotalFare(-3,2),0);
+        Assert.assertEquals(-1, calculateFare.getTotalFare(3,-3),0);
+    }
 }
