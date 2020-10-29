2342b3c1eac91d8e1198f99ddde82342368f0602
Ard2238 <abhishekrdas28@gmail.com>
2020-10-29 06:42:50 +0530

    Added Code and tests to calculate total fare for multiple rides

--- src/main/java/CalculateFare.java
+++ src/main/java/CalculateFare.java
@@ -1,4 +1,4 @@
-public class CalculateFare {
+public class CalculateFare{
     private static final double COST_PERKM = 10.0;
     private static final int COST_PERUNITTIME = 1;
 
@@ -6,5 +6,13 @@ public class CalculateFare {
         if(dist < 0 || time < 0)
             return -1;
         return (dist * COST_PERKM + time * COST_PERUNITTIME) < 5 ? 5 : dist * COST_PERKM + time * COST_PERUNITTIME;
+    }
+
+    public double getTotalFare(Ride[] ride) {
+        double sum = 0;
+        for(Ride r : ride){
+            sum = sum + this.getTotalFare(r.getDist(), r.getTime());
+        }
+        return sum;
     }
 }
--- src/test/java/InvoiceTest.java
+++ src/test/java/InvoiceTest.java
@@ -2,12 +2,26 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceTest {
+    CalculateFare calculateFare = new CalculateFare();
+
     @Test
     public void testCalculateTotalFare(){
-        CalculateFare calculateFare = new CalculateFare();
         Assert.assertEquals(45, calculateFare.getTotalFare(3.5,10), 0);
         Assert.assertEquals(5, calculateFare.getTotalFare(0.3,1),0);
+    }
+
+    @Test
+    public void testCalculateFareForInvalidInputs(){
         Assert.assertEquals(-1, calculateFare.getTotalFare(-3,2),0);
         Assert.assertEquals(-1, calculateFare.getTotalFare(3,-3),0);
+    }
+
+    @Test
+    public void testCalculateFare_ForMultipleRides(){
+        Ride[] ride = {new Ride(3.5, 10),
+                        new Ride(0.3,1),
+                        new Ride(3,2)};
+        Assert.assertEquals(82,calculateFare.getTotalFare(ride),0);
+
     }
 }
